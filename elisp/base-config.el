438e8f1ef309d13f6a3d5e42ac517159ad7ae26f
gproessl <gerrit.proessl@gmail.com>
2020-03-03 17:55:04 +0100

    update: changed space indent to tab indent

--- elisp/base-config.el
+++ elisp/base-config.el
@@ -1,20 +1,22 @@
 ;; tab settings
-(electric-indent-mode 0) ; turn off all autoindentation
-(setq-default indent-tabs-mode nil)
-(setq tab-width 2)
-(setq default-tab-width 2)
+; (electric-indent-mode 0) ; turn off all autoindentation
+(setq-default indent-tabs-mode t)
+(setq tab-width 4)
+(setq default-tab-width 4)
 (setq-default tab-always-indent t)
-(setq tab-stop-list (number-sequence 2 120 2))
+;(setq tab-stop-list (number-sequence 2 120 2))
 ;(defun tab-insert-whitespace ()
 ;  "Insert two whitspace chars"
 ;  (interactive)
 ;  (insert "  "))
 ;(global-set-key (kbd "TAB") 'tab-insert-whitespace)
 ;(global-set-key (kbd "<tab>") 'tab-insert-whitespace)
+
 ;; c
-(setq-default c-basic-offset 2)
+(setq-default c-basic-offset 4)
+
 ;; rust
-(setq rust-indent-offset 2)
+; (setq rust-indent-offset 2)
 
 (add-to-list 'auto-mode-alist '("\\.h\\'" . c++-mode))
 
